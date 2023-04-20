e9b93aa82ac3d36ce679acae5d419f456ca435a5
99winnmin <ojysep9987@gmail.com>
2023-04-20 14:15:03 +0900

    [Feat] UUID 기반 기기 연동 api 추가

---
+++ src/main/java/com/example/nftcam/api/controller/auth/AuthController.java
@@ -0,0 +1,27 @@
+package com.example.nftcam.api.controller.auth;
+
+import com.example.nftcam.api.dto.auth.request.LoginRequestDto;
+import com.example.nftcam.api.dto.auth.response.LoginResponseDto;
+import com.example.nftcam.api.service.auth.AuthService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+@RestController
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) {
+        LoginResponseDto loginResponseDto = authService.loginAndRegister(loginRequestDto);
+        return ResponseEntity.ok().body(loginResponseDto);
+    }
+}
