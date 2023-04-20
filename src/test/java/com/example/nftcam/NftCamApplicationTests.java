77adc23357340c57d57368c381083f860d855cd8
99winnmin <ojysep9987@gmail.com>
2023-04-20 13:20:06 +0900

    NFT-CAM Backend init setting

---
+++ src/test/java/com/example/nftcam/NftCamApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.example.nftcam;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class NftCamApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
