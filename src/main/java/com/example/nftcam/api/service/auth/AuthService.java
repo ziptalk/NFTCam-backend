5a277266e6286758d1862d5ed968a165f1a2318d
99winnmin <ojysep9987@gmail.com>
2023-04-20 14:17:11 +0900

    [Feat] UUID 기반 기기 연동 비지니스 로직 추가

---
+++ src/main/java/com/example/nftcam/api/service/auth/AuthService.java
@@ -0,0 +1,37 @@
+package com.example.nftcam.api.service.auth;
+
+import com.example.nftcam.api.dto.auth.request.LoginRequestDto;
+import com.example.nftcam.api.dto.auth.response.LoginResponseDto;
+import com.example.nftcam.api.entity.token.UserToken;
+import com.example.nftcam.api.entity.token.UserTokenRepository;
+import com.example.nftcam.api.entity.user.User;
+import com.example.nftcam.api.entity.user.UserRepository;
+import com.example.nftcam.filter.jwt.JwtTokenUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository userRepository;
+    private final UserTokenRepository userTokenRepository;
+    private final JwtTokenUtil jwtTokenUtil;
+
+    @Transactional
+    public LoginResponseDto loginAndRegister(LoginRequestDto loginRequestDto) {
+        User user = userRepository.findByUuid(loginRequestDto.getUuid())
+                            .orElseGet(() -> userRepository.save(loginRequestDto.toEntity()));
+
+        String accessToken = jwtTokenUtil.generateAccessToken(user.getId(), user.getRole(), user.getUuid(), true);
+        String refreshToken = jwtTokenUtil.generateRefreshToken(user.getId(), user.getRole(), user.getUuid(), true);
+        userTokenRepository.findByUserId(user.getId())
+                .ifPresentOrElse(
+                        userToken -> userToken.updateRefreshToken(refreshToken),
+                        () -> userTokenRepository.save(UserToken.builder().user(user).refreshToken(refreshToken).build())
+                );
+        return LoginResponseDto.of(accessToken, refreshToken);
+    }
+}
