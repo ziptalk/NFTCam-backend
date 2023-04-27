0f7c45d74a8e403ffffeba0f1241c72d4c70a8d1
99winnmin <ojysep9987@gmail.com>
2023-04-27 18:05:11 +0900

    [Feat] Material 생성 API 구현

--- src/main/java/com/example/nftcam/api/controller/material/MaterialController.java
+++ src/main/java/com/example/nftcam/api/controller/material/MaterialController.java
@@ -1,18 +1,23 @@
 package com.example.nftcam.api.controller.material;
 
+import com.example.nftcam.api.dto.material.request.MaterialSaveRequestDto;
 import com.example.nftcam.api.dto.material.response.MaterialCardResponseDto;
 import com.example.nftcam.api.dto.material.response.MaterialDetailResponseDto;
 import com.example.nftcam.api.dto.util.DataResponseDto;
 import com.example.nftcam.api.entity.user.details.UserAccount;
 import com.example.nftcam.api.service.material.MaterialService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -23,12 +28,15 @@ import java.util.List;
 public class MaterialController {
     private final MaterialService materialService;
 
-//    @PostMapping(value = "/new")
-//    public ResponseEntity<?> createMaterial(@AuthenticationPrincipal UserAccount userAccount,
-//                                            @RequestPart MultipartFile image,
-//                                            @RequestPart String dto) {
-//        return ResponseEntity.ok().body(materialService.createMaterial(userAccount));
-//    }
+    @PostMapping(value = "/new")
+    public ResponseEntity<?> createMaterial(@AuthenticationPrincipal UserAccount userAccount,
+                                            @RequestPart MultipartFile image,
+                                            @RequestPart String dto) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        MaterialSaveRequestDto materialSaveRequestDto = objectMapper.readValue(dto, MaterialSaveRequestDto.class);
+        materialService.createMaterial(userAccount, image, materialSaveRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 
 
     @GetMapping(value = "/list")
