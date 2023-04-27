1044967158cb2eac877d6d591fd8dca70ad21be8
99winnmin <ojysep9987@gmail.com>
2023-04-27 14:40:50 +0900

    [Feat] Material list, detail 정보 받기 비지니스 로직 구현

--- src/main/java/com/example/nftcam/api/service/material/MaterialService.java
+++ src/main/java/com/example/nftcam/api/service/material/MaterialService.java
@@ -1,14 +1,21 @@
 package com.example.nftcam.api.service.material;
 
-import com.example.nftcam.api.dto.material.request.MaterialSaveRequestDto;
+import com.example.nftcam.api.dto.material.response.MaterialCardResponseDto;
+import com.example.nftcam.api.dto.material.response.MaterialDetailResponseDto;
+import com.example.nftcam.api.dto.util.DataResponseDto;
 import com.example.nftcam.api.entity.material.MaterialRepository;
 import com.example.nftcam.api.entity.user.UserRepository;
 import com.example.nftcam.api.entity.user.details.UserAccount;
-import com.example.nftcam.utils.LocationConvertion;
+import com.example.nftcam.exception.custom.CustomException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -16,15 +23,25 @@ import org.springframework.transaction.annotation.Transactional;
 public class MaterialService {
     private final UserRepository userRepository;
     private final MaterialRepository materialRepository;
-    private final LocationConvertion locationConvertion;
 
-    @Transactional
-    public void saveMaterial(UserAccount userAccount, MaterialSaveRequestDto materialSaveRequestDto) {
+    @Transactional(readOnly = true)
+    public DataResponseDto<List<MaterialCardResponseDto>> getMaterialCardList(UserAccount userAccount, Long cursor, Pageable pageable) {
+        List<MaterialCardResponseDto> materialCardResponseDtos = materialRepository.findAllByUserIdWithPaging(userAccount.getUserId(), cursor, pageable).stream()
+                .map(MaterialCardResponseDto::of)
+                .collect(Collectors.toList());
 
+        return DataResponseDto.<List<MaterialCardResponseDto>>builder()
+                .data(materialCardResponseDtos)
+                .build();
     }
 
-    public void test(){
-        // 경도, 위도로 입력
-        locationConvertion.coordToAddr("37.496496","127.029320");
+    @Transactional
+    public DataResponseDto<MaterialDetailResponseDto> getMaterialDetail(UserAccount userAccount, Long materialId) {
+        MaterialDetailResponseDto materialDetailResponseDto = MaterialDetailResponseDto.of(materialRepository.findById(materialId)
+                .orElseThrow(() -> CustomException.builder().httpStatus(HttpStatus.BAD_REQUEST).message("존재하지 않는 data 입니다.").build()));
+
+        return DataResponseDto.<MaterialDetailResponseDto>builder()
+                .data(materialDetailResponseDto)
+                .build();
     }
 }
