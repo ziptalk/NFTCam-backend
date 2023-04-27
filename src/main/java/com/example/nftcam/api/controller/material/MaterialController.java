7d24e9a40f464118dcfb9bb329cd5b73c9786a68
99winnmin <ojysep9987@gmail.com>
2023-04-27 14:40:17 +0900

    [Feat] Material list, detail 정보 받기 API 구현

--- src/main/java/com/example/nftcam/api/controller/material/MaterialController.java
+++ src/main/java/com/example/nftcam/api/controller/material/MaterialController.java
@@ -1,13 +1,20 @@
 package com.example.nftcam.api.controller.material;
 
+import com.example.nftcam.api.dto.material.response.MaterialCardResponseDto;
+import com.example.nftcam.api.dto.material.response.MaterialDetailResponseDto;
+import com.example.nftcam.api.dto.util.DataResponseDto;
+import com.example.nftcam.api.entity.user.details.UserAccount;
 import com.example.nftcam.api.service.material.MaterialService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.lang.Nullable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RequestMapping("/api/material")
@@ -16,12 +23,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class MaterialController {
     private final MaterialService materialService;
 
-    @PostMapping("/convertion")
-    public ResponseEntity<?> saveMaterial(
-           // @AuthenticationPrincipal UserAccount userAccount, @RequestBody MaterialSaveRequestDto materialSaveRequestDto
-    ) {
-//        materialService.saveMaterial(userAccount, materialSaveRequestDto);
-        materialService.test();
-        return ResponseEntity.ok(HttpStatus.CREATED);
+    @GetMapping(value = "/list")
+    public ResponseEntity<DataResponseDto<List<MaterialCardResponseDto>>> getHomePageList
+            (
+                    @AuthenticationPrincipal UserAccount userAccount,
+                    @Nullable @RequestParam Long cursor,
+                    @PageableDefault(size = 6) Pageable pageable
+            )
+    {
+        return ResponseEntity.ok().body(materialService.getMaterialCardList(userAccount, cursor, pageable));
+    }
+
+    @GetMapping(value = "/{materialId}")
+    public ResponseEntity<DataResponseDto<MaterialDetailResponseDto>> getMaterialDetail(@AuthenticationPrincipal UserAccount userAccount, @PathVariable Long materialId) {
+        return ResponseEntity.ok().body(materialService.getMaterialDetail(userAccount, materialId));
     }
 }
