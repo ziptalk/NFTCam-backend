532641dfde2d65d92e8c8259441628c157c43a36
99winnmin <ojysep9987@gmail.com>
2023-04-27 14:42:55 +0900

    [Feat] server default data responseDto

---
+++ src/main/java/com/example/nftcam/api/dto/util/DataResponseDto.java
@@ -0,0 +1,18 @@
+package com.example.nftcam.api.dto.util;
+
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DataResponseDto<T> {
+    private T data;
+
+    @Builder
+    public DataResponseDto(T data) {
+        this.data = data;
+    }
+}
