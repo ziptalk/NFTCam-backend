29f9df67b7683c1c1369c9483ea7652f864a82fa
99winnmin <ojysep9987@gmail.com>
2023-04-27 14:42:04 +0900

    [Feat] Material list 커서 기반 최신순 pagination

--- src/main/java/com/example/nftcam/api/entity/material/MaterialRepository.java
+++ src/main/java/com/example/nftcam/api/entity/material/MaterialRepository.java
@@ -1,6 +1,10 @@
 package com.example.nftcam.api.entity.material;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MaterialRepository extends JpaRepository<Material, Long> {
+import java.util.List;
+
+public interface MaterialRepository extends JpaRepository<Material, Long>, MaterialRepositoryCustom {
+    List<Material> findAllByUserIdWithPaging(Long userId, Long cursorId, Pageable pageable);
 }
