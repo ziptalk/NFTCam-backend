e25c9caf80b45f403696d673a75da1973ca91442
99winnmin <ojysep9987@gmail.com>
2023-05-11 14:51:14 +0900

    [Feat] Material 소유자 증명 JPA 추가

--- src/main/java/com/example/nftcam/api/entity/material/MaterialRepository.java
+++ src/main/java/com/example/nftcam/api/entity/material/MaterialRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MaterialRepository extends JpaRepository<Material, Long>, MaterialRepositoryCustom {
     List<Material> findAllByUserIdWithPaging(Long userId, Long cursorId, Pageable pageable);
+
+    Optional<Material> findByIdAndUser_Id(Long materialId, Long userId);
 }
