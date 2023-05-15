f8839e485152a3c330004dcf140a5278d84b99e8
99winnmin <ojysep9987@gmail.com>
2023-05-15 13:25:14 +0900

    [Feat] token refresh API 비지니스 코드 추가

--- src/main/java/com/example/nftcam/api/service/auth/AuthService.java
+++ src/main/java/com/example/nftcam/api/service/auth/AuthService.java
@@ -1,14 +1,18 @@
 package com.example.nftcam.api.service.auth;
 
-import com.example.nftcam.api.dto.auth.request.LoginRequestDto;
-import com.example.nftcam.api.dto.auth.response.LoginResponseDto;
+import com.example.nftcam.api.dto.auth.request.IssueTokenRequestDto;
+import com.example.nftcam.api.dto.auth.request.ReissueTokenRequestDto;
+import com.example.nftcam.api.dto.auth.response.IssueTokenResponseDto;
 import com.example.nftcam.api.entity.token.UserToken;
 import com.example.nftcam.api.entity.token.UserTokenRepository;
 import com.example.nftcam.api.entity.user.User;
 import com.example.nftcam.api.entity.user.UserRepository;
+import com.example.nftcam.exception.custom.ExpiredTokenException;
+import com.example.nftcam.exception.custom.NotFoundException;
 import com.example.nftcam.filter.jwt.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,9 +25,9 @@ public class AuthService {
     private final JwtTokenUtil jwtTokenUtil;
 
     @Transactional
-    public LoginResponseDto loginAndRegister(LoginRequestDto loginRequestDto) {
-        User user = userRepository.findByUuid(loginRequestDto.getUuid())
-                            .orElseGet(() -> userRepository.save(loginRequestDto.toEntity()));
+    public IssueTokenResponseDto loginAndRegister(IssueTokenRequestDto issueTokenRequestDto) {
+        User user = userRepository.findByUuid(issueTokenRequestDto.getUuid())
+                            .orElseGet(() -> userRepository.save(issueTokenRequestDto.toEntity()));
 
         String accessToken = jwtTokenUtil.generateAccessToken(user.getId(), user.getRole(), user.getUuid(), true);
         String refreshToken = jwtTokenUtil.generateRefreshToken(user.getId(), user.getRole(), user.getUuid(), true);
@@ -32,11 +36,21 @@ public class AuthService {
                         userToken -> userToken.updateRefreshToken(refreshToken),
                         () -> userTokenRepository.save(UserToken.builder().user(user).refreshToken(refreshToken).build())
                 );
-        return LoginResponseDto.of(accessToken, refreshToken);
+        return IssueTokenResponseDto.of(accessToken, refreshToken);
     }
 
-//    @Transactional
-//    public LoginResponseDto reissue(LoginRequestDto loginRequestDto) {
-//
-//    }
+    @Transactional
+    public IssueTokenResponseDto reissue(ReissueTokenRequestDto reissueTokenRequestDto) {
+        UserToken userToken = userTokenRepository.findUserTokenByRefreshToken(reissueTokenRequestDto.getRefreshToken())
+                .orElseThrow(() -> NotFoundException.builder().httpStatus(HttpStatus.NOT_FOUND).message("요청하신 토큰 정보를 찾을 수 없습니다.").build());
+
+        if (jwtTokenUtil.isValidToken(reissueTokenRequestDto.getRefreshToken()) && jwtTokenUtil.isExpiredToken(reissueTokenRequestDto.getRefreshToken())) {
+            String accessToken = jwtTokenUtil.generateAccessToken(userToken.getUser().getId(), userToken.getUser().getRole(), userToken.getUser().getUuid(), true);
+            String refreshToken = jwtTokenUtil.generateRefreshToken(userToken.getUser().getId(), userToken.getUser().getRole(), userToken.getUser().getUuid(), true);
+            userToken.updateRefreshToken(refreshToken);
+            return IssueTokenResponseDto.of(accessToken, refreshToken);
+        } else {
+            throw ExpiredTokenException.builder().httpStatus(HttpStatus.UNAUTHORIZED).message("refresh 토큰이 만료되었습니다. 다시 로그인해주세요.").build();
+        }
+    }
 }
