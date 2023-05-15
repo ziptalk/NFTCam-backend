cb1898738de388e956926ff181b241a34d4b849e
99winnmin <ojysep9987@gmail.com>
2023-05-15 13:27:55 +0900

    [Feat] token 재발행 요청 DTO

---
+++ src/main/java/com/example/nftcam/api/dto/auth/request/ReissueTokenRequestDto.java
@@ -0,0 +1,11 @@
+package com.example.nftcam.api.dto.auth.request;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReissueTokenRequestDto {
+    private String refreshToken;
+}
