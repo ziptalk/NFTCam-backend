b6e6853f66a2a7f4cb22d478b705557aa1f2c4c3
99winnmin <ojysep9987@gmail.com>
2023-06-22 16:56:27 +0900

    [Feat] point api dto

---
+++ src/main/java/com/example/nftcam/api/dto/point/response/PointChargeResponseDto.java
@@ -0,0 +1,12 @@
+package com.example.nftcam.api.dto.point.response;
+
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PointChargeResponseDto {
+    private Long point;
+
+}
