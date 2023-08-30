2ffc82f478a268031e669d2c1965914cf7354736
99winnmin <ojysep9987@gmail.com>
2023-08-30 16:55:08 +0900

    :sparkles: [Fix] Material detail API -> 어떤 chain으로 민팅되었는지 정보 추가

--- src/main/java/com/example/nftcam/api/dto/material/response/MaterialDetailResponseDto.java
+++ src/main/java/com/example/nftcam/api/dto/material/response/MaterialDetailResponseDto.java
@@ -1,5 +1,6 @@
 package com.example.nftcam.api.dto.material.response;
 
+import com.example.nftcam.api.entity.material.ChainType;
 import com.example.nftcam.api.entity.material.Material;
 import com.example.nftcam.api.entity.material.MintState;
 import lombok.AccessLevel;
@@ -18,17 +19,19 @@ private Long materialId;
     private String source;
     private String nftId;
     private MintState isMinting;
+    private ChainType chainType;
     private String device;
     private String address;
     private String date;
 
     @Builder
-    public MaterialDetailResponseDto(Long materialId, String title, String source, String nftId, MintState isMinting, String device, String address, String date) {
+    public MaterialDetailResponseDto(Long materialId, String title, String source, String nftId, MintState isMinting, ChainType chainType, String device, String address, String date) {
         this.materialId = materialId;
         this.title = title;
         this.source = source;
         this.nftId = nftId;
         this.isMinting = isMinting;
+        this.chainType = chainType;
         this.device = device;
         this.address = address;
         this.date = date;
@@ -42,6 +45,7 @@ private Long materialId;
                 .source(material.getSource())
                 .nftId(Optional.ofNullable(material.getNftId()).orElseGet(() -> "미발행"))
                 .isMinting(material.getIsMinting())
+                .chainType(material.getChainType() == null ? null : material.getChainType())
                 .device(material.getDevice())
                 .address(material.getAddress())
                 .date(material.getTakenAt().format(formatter))
